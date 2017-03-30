0c8a6c4f3f82ed702fbf9b87c82514f95dffab28
kita <kita@kita-PC>
2017-03-30 16:10:44 +0900

    commit yu

---
+++ rodo/docs/rodo.sql
@@ -0,0 +1,56 @@
+drop table;
+drop table;
+drop table;
+drop table;
+drop table;
+drop table;
+drop table;
+drop table;
+drop table;
+drop table;
+drop table;
+drop table;
+drop table;
+drop table;
+drop table;
+drop table;
+
+
+create table (
+);
+create table (
+);
+create table (
+);
+create table (
+);
+create table (
+);
+create table (
+);
+create table (
+);
+create table (
+);
+create table (
+);
+create table (
+);
+create table (
+);
+create table (
+);
+create table (
+);
+create table (
+);
+create table (
+);
+create table (
+);
+
+
+create sequence gps_seq start with 1 increment by 1;
+create sequence photo_seq start with 1 increment by 1;
+create sequence video_seq start with 1 increment by 1;
+create sequence free_seq start with 1 increment by 1;
